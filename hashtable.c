c97e0593eb6124566317cea5818c41e479dacba6
Steven Lee <geekerlw@gmail.com>
2018-06-23 20:02:45 +0000

    procfs unit test pass

--- hashtable.c
+++ hashtable.c
@@ -12,13 +12,22 @@
  * copies or substantial portions of the Software.
  */
 
+#include <linux/string.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/slab.h>
+#include <linux/syscalls.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 #include <linux/list.h>
+#include <linux/slab.h>
 
-#define HASH_TABLE_SIZE (8)
+#define HASH_TABLE_SIZE	(8)
+#define HASH_KEY_MAX_LEN	(256)
+#define HASH_VALUE_MAX_LEN	(512)
+#define MAX_BUF_SIZE (HASH_KEY_MAX_LEN + HASH_VALUE_MAX_LEN + 16)
+
+#define sys_call_num (223)
 
 typedef char* hash_key_t;
 typedef char* hash_value_t;
@@ -30,6 +39,14 @@ typedef struct ht_data{
 }ht_data_t;
 
 struct hlist_head g_hash_table[HASH_TABLE_SIZE];
+
+static unsigned long *g_syscall_addr[3];
+static unsigned long **g_syscall_table_addr;
+
+static const char *proc_node_name = "hashtable";
+static struct proc_dir_entry *proc_node_entry;
+
+static char proc_msg[MAX_BUF_SIZE];;
 
 // BKDRHash
 static unsigned int ht_hash_create(hash_key_t key)
@@ -82,7 +99,7 @@ void ht_data_add(const hash_key_t key, const hash_value_t value)
 	data->value = (char *) kmalloc(sizeof(char) * (strlen(value) + 1), GFP_KERNEL);
 
 	if(data == NULL || data->key == NULL || data->value == NULL) {
-		printk("debug: kmalloc failed\n");
+		printk("hashtable: kmalloc failed\n");
 		return;
 	}
 
@@ -104,7 +121,6 @@ void ht_data_remove(const hash_key_t key)
 	ht_data_t *data;
 
 	if(!ht_search_by_key(key, &index, &data)) {
-		// do not find the key
 		return;
 	}
 
@@ -128,7 +144,6 @@ int ht_data_query(const hash_key_t key, hash_value_t *value) {
 	ht_data_t *data;
 
 	if(!ht_search_by_key(key, &index, &data)) {
-		// node not found
 		return -1;
 	}
 
@@ -138,41 +153,127 @@ int ht_data_query(const hash_key_t key, hash_value_t *value) {
 }
 EXPORT_SYMBOL(ht_data_query);
 
-/*
-static void hashtable_unit_test(void)
+static void syscall_addr_rw(unsigned long address)
 {
-	char key[] = "key1";
-	char value[] = "value1";
-
-	char *p = (char *) kmalloc(sizeof(char), GFP_KERNEL);
-	char *p1 = (char *) kmalloc(sizeof(char), GFP_KERNEL);
-
-	// insert test
-	ht_data_add(key, value);
-	printk("debug: add the key: %s, value: %s\n", key, value);
-
-	// query test
-	ht_data_query(key, &p);
-	printk("debug: query out the data: %s\n", p);
-	kfree(p);
-
-	// remove test
-	if(ht_data_remove(key) == 0) {
-		printk("debug: remove the key: %s\n", key);
-		// query again
-		if(ht_data_query(key, &p1) == 0) {
-			printk("debug: query out the key: %s, value: %s\n", key, p1);
-		} else {
-			printk("debug: query failed, not found\n");
-		}
-	} else {
-		printk("debug: remove the key failed, key: %s\n", key);
+	unsigned int level;
+	pte_t *p = lookup_address(address, &level);
+	if(p->pte & ~_PAGE_RW) {
+		p->pte |= _PAGE_RW;
 	}
-	kfree(p1);	
 
 	return;
 }
-*/
+
+static void syscall_addr_ro(unsigned long address)
+{
+	unsigned int level;
+	pte_t *p = lookup_address(address, &level);
+	p->pte &= ~_PAGE_RW;
+
+	return;
+}
+
+static unsigned long **syscall_table_address_get(void)
+{
+	unsigned long int offset = PAGE_OFFSET;
+	unsigned long **sct;
+
+	while (offset < ULLONG_MAX) {
+		sct = (unsigned long **)offset;
+
+		if (sct[__NR_close] == (unsigned long *) ksys_close)
+			return sct;
+
+		offset += sizeof(void *);
+	}
+
+	return NULL;
+}
+
+static int syscall_init_module(void)
+{
+	g_syscall_table_addr = syscall_table_address_get();
+	if (g_syscall_table_addr == NULL) {
+		printk("hashtable: failed to get sys call table address\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int syscall_cleanup_module(void)
+{
+
+	return 0;
+}
+
+// read proc in user space
+static ssize_t proc_node_read(struct file *file, char __user *buffer, size_t count, loff_t *pos)
+{
+	ssize_t len = strlen(proc_msg);
+
+	return simple_read_from_buffer(buffer, count, pos, proc_msg, len);
+}
+
+// write proc in user space
+static ssize_t proc_node_write(struct file *file, const char __user *buffer, size_t count, loff_t *pos)
+{
+	char msg[MAX_BUF_SIZE] = { 0 };
+	char args[MAX_BUF_SIZE] = { 0 };
+	ssize_t len;
+	const char *delim = " ";
+	hash_key_t key;
+	hash_value_t value;
+	char *s, *p = NULL;
+
+	// clear proc msg
+	memset(proc_msg, 0, sizeof(proc_msg));
+
+	len = simple_write_to_buffer(msg, sizeof(msg), pos, buffer, count);
+	memcpy(args, msg + 4, count - 5); // no need '\n'
+	s = args;
+
+	// add key-value
+	if(strncmp(msg, "add", strlen("add")) == 0) {
+		for(p = strsep(&s, delim); p != NULL; p = strsep(&s, delim)) {
+			key = p;
+			value = strsep(&s, delim);
+			printk("hashtable: add key: %s, value: %s\n", key, value);
+			ht_data_add(key, value);
+			sprintf(proc_msg, "add key: %s, value: %s\n", key, value);
+		}
+	}
+	// delete key
+	else if (strncmp("del", msg, strlen("del")) == 0) {
+		for(p = strsep(&s, delim); p != NULL; p = strsep(&s, delim)) {
+			key = p;
+			printk("hashtable: del key: %s\n", key);
+			ht_data_remove(key);
+			sprintf(proc_msg, "del key: %s\n", key);
+		}
+	}
+	// query key
+	else if (strncmp("get", msg, strlen("get")) == 0) {
+		for(p = strsep(&s, delim); p != NULL; p = strsep(&s, delim)) {
+			char *value;
+			key = p;
+			if(ht_data_query(key, &value) == 0 ) {
+				printk("hashtable: found the key: %s, value: %s\n", key, value);
+				sprintf(proc_msg, "query out the key: %s, value: %s\n", key, value);
+			} else {
+				printk("hashtable: not found the key: %s\n", key);
+				sprintf(proc_msg, "query failed, no such key: %s\n", key);
+			}
+		}
+	}
+
+	return len;
+}
+
+static struct file_operations proc_node_fops = {
+	.read = proc_node_read,
+	.write = proc_node_write,
+};
 
 static int __init hashtable_init(void)
 {
@@ -183,7 +284,14 @@ static int __init hashtable_init(void)
 		INIT_HLIST_HEAD(g_hash_table + i);
 	}
 
-	//hashtable_unit_test();
+	// system call replace
+	//syscall_init_module();
+
+	// procfs node create
+	if((proc_node_entry = proc_create(proc_node_name, S_IFREG | S_IRUGO | S_IWUGO, NULL, &proc_node_fops)) == NULL) {
+		printk("hashtable: failed to create proc fs node: %s\n", proc_node_name);
+		return 0;
+	}
 	
 	printk("hashtable: module init success\n");
 
@@ -195,6 +303,9 @@ static void __exit hashtable_exit(void)
 	unsigned int i;
 	ht_data_t *data;
 	struct hlist_node *tmp_hnode;
+
+	// proc node clear
+	proc_remove(proc_node_entry);
 
 	// hash table clear
 	for(i = 0; i < HASH_TABLE_SIZE; i++) {
