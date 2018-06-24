202cc5cd66c24cf04354f9459c6598f2daaaa89d
Steven Lee <geekerlw@gmail.com>
2018-06-24 20:14:36 +0000

    syscall function support, but arguments not passed

--- hashtable.c
+++ hashtable.c
@@ -17,6 +17,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
+#include <linux/kallsyms.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
 #include <linux/list.h>
@@ -27,7 +28,13 @@
 #define HASH_VALUE_MAX_LEN	(512)
 #define MAX_BUF_SIZE (HASH_KEY_MAX_LEN + HASH_VALUE_MAX_LEN + 16)
 
-#define sys_call_num (223)
+#define SYSCALL_FAKE_NUM	(223)
+
+enum HASH_CMD {
+	HASH_ADD,
+	HASH_DEL,
+	HASH_GET
+};
 
 typedef char* hash_key_t;
 typedef char* hash_value_t;
@@ -40,13 +47,16 @@ typedef struct ht_data{
 
 struct hlist_head g_hash_table[HASH_TABLE_SIZE];
 
-static unsigned long *g_syscall_addr[3];
-static unsigned long **g_syscall_table_addr;
+static unsigned long g_syscall_fake_addr;
+static void *g_syscall_fake_func;
+static unsigned long g_syscall_table_addr;
+module_param(g_syscall_table_addr, ulong, 0);
+MODULE_PARM_DESC(g_syscall_table_addr, "syscall table address");
 
 static const char *proc_node_name = "hashtable";
 static struct proc_dir_entry *proc_node_entry;
 
-static char proc_msg[MAX_BUF_SIZE];;
+static char proc_msg[MAX_BUF_SIZE];
 
 // BKDRHash
 static unsigned int ht_hash_create(hash_key_t key)
@@ -153,7 +163,8 @@ int ht_data_query(const hash_key_t key, hash_value_t *value) {
 }
 EXPORT_SYMBOL(ht_data_query);
 
-static void syscall_addr_rw(unsigned long address)
+// page permission helper
+static void page_addr_rw(unsigned long address)
 {
 	unsigned int level;
 	pte_t *p = lookup_address(address, &level);
@@ -164,7 +175,7 @@ static void syscall_addr_rw(unsigned long address)
 	return;
 }
 
-static void syscall_addr_ro(unsigned long address)
+static void page_addr_ro(unsigned long address)
 {
 	unsigned int level;
 	pte_t *p = lookup_address(address, &level);
@@ -173,36 +184,70 @@ static void syscall_addr_ro(unsigned long address)
 	return;
 }
 
-static unsigned long **syscall_table_address_get(void)
+asmlinkage long sys_hashtable(int cmd, const hash_key_t key, hash_value_t *value)
 {
-	unsigned long int offset = PAGE_OFFSET;
-	unsigned long **sct;
+	int ret = 0;
+	printk("hashtable: call the hashtable via syscall\n");
 
-	while (offset < ULLONG_MAX) {
-		sct = (unsigned long **)offset;
-
-		if (sct[__NR_close] == (unsigned long *) ksys_close)
-			return sct;
-
-		offset += sizeof(void *);
+	switch(cmd) {
+		case HASH_ADD:
+			printk("hashtable: run add, key: %s, value: %s\n", key, *value);
+			ht_data_add(key, *value);
+			break;
+		case HASH_DEL:
+			printk("hashtable: run del, key: %s\n", key);
+			ht_data_remove(key);
+			break;
+		case HASH_GET:
+			printk("hashtable: run get, key: %s\n", key);
+			ret = ht_data_query(key, value);
+			if (ret == 0) {
+				printk("hashtable: query out the value: %s\n", *value);
+			} else {
+				printk("hashtable: not found the key: %s\n", key);
+			}
+			break;
+		default:
+			break;
 	}
 
-	return NULL;
+	return (long)ret;
 }
 
 static int syscall_init_module(void)
 {
-	g_syscall_table_addr = syscall_table_address_get();
-	if (g_syscall_table_addr == NULL) {
-		printk("hashtable: failed to get sys call table address\n");
+	unsigned long *p;
+	// get the syscall table address
+	g_syscall_table_addr = kallsyms_lookup_name("sys_call_table");
+	if (g_syscall_table_addr == 0) {
+		printk("hashtable: not found the sys_call_table symbol\n");
 		return -1;
 	}
+
+	// get fake address
+	g_syscall_fake_addr = g_syscall_table_addr + sizeof(long) * SYSCALL_FAKE_NUM;
+
+	// store the old func
+	p = (unsigned long *)g_syscall_fake_addr;
+	g_syscall_fake_func = (void *)(*p);
+
+	// fake to own func
+	page_addr_rw(g_syscall_fake_addr);
+	*p = (unsigned long)sys_hashtable;
+	page_addr_ro(g_syscall_fake_addr);
 
 	return 0;
 }
 
 static int syscall_cleanup_module(void)
 {
+	unsigned long *p;
+	p = (unsigned long *)g_syscall_fake_addr;
+
+	// revert fake addr
+	page_addr_rw(g_syscall_fake_addr);
+	*p = (unsigned long)g_syscall_fake_func;
+	page_addr_ro(g_syscall_fake_addr);
 
 	return 0;
 }
@@ -285,7 +330,7 @@ static int __init hashtable_init(void)
 	}
 
 	// system call replace
-	//syscall_init_module();
+	syscall_init_module();
 
 	// procfs node create
 	if((proc_node_entry = proc_create(proc_node_name, S_IFREG | S_IRUGO | S_IWUGO, NULL, &proc_node_fops)) == NULL) {
@@ -306,6 +351,9 @@ static void __exit hashtable_exit(void)
 
 	// proc node clear
 	proc_remove(proc_node_entry);
+
+	// system call revert
+	syscall_cleanup_module();
 
 	// hash table clear
 	for(i = 0; i < HASH_TABLE_SIZE; i++) {
